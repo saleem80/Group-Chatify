e82d4983ebb3cca50946be452653885831774624
Saleem Khan <111304283+saleem80@users.noreply.github.com>
2022-10-14 12:56:21 +0530

    Updated to 2.0

---
+++ README.md
@@ -0,0 +1,54 @@
+<div align="center">
+
+# Chatify
+
+### Personal Chat Room or Workspace to share resources and hangout with friends.
+
+### https://chatify-49.web.app/
+
+<div align="center">
+  <img alt="Demo" src="./Assets/chatify.png" />
+</div>
+
+</div>
+
+## Build With
+
+- `React.js`
+- `Firebase`
+- `Material UI`
+- `React Icons`
+
+## Features:
+
+- Easy SignIn with Google
+- Create Rooms
+- Realtime group messaging with image sharing
+- Supports Emoji
+- Reaction on messages
+
+## Installation and Usage
+
+### Clone this repository
+
+`git clone https://github.com/soumyajit4419/Chatify.git` <br/>
+`cd chatify`
+
+### Install Dependencies
+
+`npm install`
+
+In the project directory, you can run:
+
+### npm start
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### npm run build
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
---
+++ src/Components/SignUp.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { FcGoogle } from "react-icons/fc";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import loginImg from "../Assets/login.png";
+import Topography from "@material-ui/core/Typography";
+import { auth, provider } from "../Firebase/Firebase";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    boxShadow: "0 0 15px rgb(7 15 63 / 33%)",
+    backgroundColor: "#171c30",
+    color: "white",
+  },
+  paper: {
+    marginTop: theme.spacing(10),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingBottom: "25px",
+    paddingTop: "35px",
+  },
+  mainImg: {
+    width: "100%",
+    height: "auto",
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    color: "#d9d9d9",
+  },
+}));
+
+function SignUp() {
+  const classes = useStyles();
+
+  const login = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((res) => {
+        console.log("Success");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <Container component="div" maxWidth="xs" className={classes.root}>
+      <div className={classes.paper}>
+        <img src={loginImg} className={classes.mainImg} alt="signup img" />
+        <Topography variant="h4" style={{ paddingTop: "15px" }}>
+          Sign In To Chatify
+        </Topography>
+        <Button
+          variant="outlined"
+          color="primary"
+          className={classes.submit}
+          startIcon={<FcGoogle />}
+          onClick={login}
+        >
+          Sign In With Google
+        </Button>
+      </div>
+    </Container>
+  );
+}
+
+export default SignUp;
